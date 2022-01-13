24c95f729f397e5fcf3342bffed992e063fd217a
HyeokSong <sssong1213@naver.com>
2022-01-13 16:23:52 +0900

    Todo#2

--- js/todo.js
+++ js/todo.js
@@ -1,6 +1,15 @@
 const toDoForm = document.getElementById("todo-form");
 const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
+const TODOS_KEY = "todos";
+let toDos = [];
+/*
+application value에 array형식으로 저장된다 텍스트이긴 하지만
+그리고 나서 다시 array형태의 텍스트를 array로 만드는 작업을 하는 것 같음
+*/
+function saveTodos() {
+    localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
+}
 
 function deleteTodo(event) {
     const li = event.target.parentElement;
@@ -23,7 +32,19 @@ function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
+    toDos.push(newTodo);
     paintTodo(newTodo);
+    saveTodos();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
+
+// 왜 savedTodos가 맨 마지막에 오는지 공부하기
+const savedTodos = localStorage.getItem(TODOS_KEY);
+if (savedTodos) {
+    const parsedTodos = JSON.parse(savedTodos);
+    toDos = parsedTodos;
+    parsedTodos.forEach(paintTodo);
+}
+//6-5  복습하기 6-6듣기전에 todo를 삭제해도 다시 복원되는 이유는 뭘까 생각하고 듣기
+// 화면에서는 지웠지만 로컬 스토리지에서 삭제하지 않았다
