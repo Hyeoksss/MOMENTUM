9040b38db6493a1b6b2d74efdff0da79858411f4
HyeokSong <sssong1213@naver.com>
2022-01-14 19:07:53 +0900

    Todo#3

--- js/todo.js
+++ js/todo.js
@@ -3,10 +3,7 @@ const toDoInput = toDoForm.querySelector("input");
 const toDoList = document.getElementById("todo-list");
 const TODOS_KEY = "todos";
 let toDos = [];
-/*
-application value에 array형식으로 저장된다 텍스트이긴 하지만
-그리고 나서 다시 array형태의 텍스트를 array로 만드는 작업을 하는 것 같음
-*/
+
 function saveTodos() {
     localStorage.setItem(TODOS_KEY, JSON.stringify(toDos));
 }
@@ -14,17 +11,20 @@ function saveTodos() {
 function deleteTodo(event) {
     const li = event.target.parentElement;
     li.remove();
+    toDos = toDos.filter((toDO) => toDO.id !== parseInt(li.id));
+    saveTodos();
 }
 
 function paintTodo(newTodo) {
     const li = document.createElement("li");
+    li.id = newTodo.id;
     const span = document.createElement("span");
+    span.innerText = newTodo.text;
     const button = document.createElement("button");
     button.innerText = "❌";
     button.addEventListener("click", deleteTodo);
     li.appendChild(span);
     li.appendChild(button);
-    span.innerText = newTodo;
     toDoList.appendChild(li);
 }
 
@@ -32,19 +32,21 @@ function handleTodoSubmit(event) {
     event.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value = "";
-    toDos.push(newTodo);
-    paintTodo(newTodo);
+    const newTodoObj = {
+        text: newTodo,
+        id: Date.now(),
+    };
+    toDos.push(newTodoObj);
+    paintTodo(newTodoObj);
     saveTodos();
 }
 
 toDoForm.addEventListener("submit", handleTodoSubmit);
 
-// 왜 savedTodos가 맨 마지막에 오는지 공부하기
 const savedTodos = localStorage.getItem(TODOS_KEY);
-if (savedTodos) {
+
+if (savedTodos !== null) {
     const parsedTodos = JSON.parse(savedTodos);
     toDos = parsedTodos;
     parsedTodos.forEach(paintTodo);
 }
-//6-5  복습하기 6-6듣기전에 todo를 삭제해도 다시 복원되는 이유는 뭘까 생각하고 듣기
-// 화면에서는 지웠지만 로컬 스토리지에서 삭제하지 않았다
