89e0677c1b06558ab72703c22a7efd4895cc9a3a
HyeokSong <sssong1213@naver.com>
2022-01-16 14:40:00 +0900

    weather#2

--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,18 @@
+const API_KEY = "3ce0fef08296048dbcb7beb0257a2152";
+
 function onGeoOk(position) {
     const lat = position.coords.latitude;
-    const lng = position.coords.longtitude;
+    const lon = position.coords.longitude;
+    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=3ce0fef08296048dbcb7beb0257a2152`;
+    console.log(url);
+    fetch(url)
+        .then((response) => response.json())
+        .then((data) => {
+            const weather = document.querySelector("#weather span:first-child");
+            const city = document.querySelector("#weather span:last-child");
+            city.innerText = data.name;
+            weather.innerText = `${data.weather[0].main} / ${data.main.temp}`;
+        });
 }
 function onGeoError() {
     alert("error");
